22bb2857191bb886f2d00a68657b449af13034ce
Luis Sandoval <luissandoval1126@gmail.com>
2020-04-19 21:39:50 -0700

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "Mastadon",
-  password: "pp95eMLJ",
-  database: "burgers_db"
+  user: "qf93bjails376xqb",
+  password: "	j5vuzjr7o1hdt61k",
+  database: "	ah65t2p8ox07zrup",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
